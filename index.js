68391800bca6a0d591fe4c517495c86e2ae775a5
lbako801 <lbako98@gmail.com>
2023-02-19 23:03:03 -0700

    made a library for team member objects instead of being in index.js, added test files

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-class Employee{
-    constructor(name, email, githubUsername) {
-      this.name = name;
-      this.email = email;
-      this.githubUsername = githubUsername;
-    }
-}
-
-class Manager extends Employee{
-    constructor(name, email, githubUsername, officeNumber) {
-      super(name, email, githubUsername);
-      this.officeNumber = officeNumber;
-    }
-}
-
-class Engineer extends Employee{
-    constructor(name, email, githubUsername, githubProfile) {
-      super(name, email, githubUsername);
-      this.githubProfile = githubProfile;
-    }
-}
-
-class Intern extends Employee{
-    constructor(name, email, githubUsername, school) {
-      super(name, email, githubUsername);
-      this.school = school;
-    }
-}
 
 // Ask the user for the number of team members
 inquirer.prompt([
@@ -43,4 +15,4 @@ inquirer.prompt([
             return true;
         },
     },
-]);
+])
